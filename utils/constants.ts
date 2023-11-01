bd2fd08178a366c179a5455a847442d981825a1a
Rodrigo Ramirez <AR20063@ues.edu.sv>
2023-11-01 16:49:27 -0600

    CreeCarpetaUtilsyConstantsyModifique

---
+++ utils/constants.ts
@@ -0,0 +1,8 @@
+export enum Routes  {
+    HOME = '/',
+    EXAMPLES_HOME =  '/examples',
+    EXAMPLES_ONE = '/examples/1',
+    EXAMPLES_TWO = '/examples/2',
+    EXAMPLES_THREE = '/examples/3',
+
+}
